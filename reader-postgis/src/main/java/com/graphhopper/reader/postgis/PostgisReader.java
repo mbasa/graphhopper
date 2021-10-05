686c6955bd5cac99c43869bcdd84e6140c3e5ce5
Mario Basa <mario.basa@gmail.com>
2021-10-05 23:44:27 +0900

    adding reader-postgis

---
+++ reader-postgis/src/main/java/com/graphhopper/reader/postgis/PostgisReader.java
@@ -0,0 +1,186 @@
+/*
+ *  Licensed to GraphHopper GmbH under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for
+ *  additional information regarding copyright ownership.
+ *
+ *  GraphHopper GmbH licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.graphhopper.reader.postgis;
+
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.storage.Graph;
+import com.graphhopper.storage.GraphHopperStorage;
+import com.graphhopper.storage.NodeAccess;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * PostgisReader takes care of reading a PostGIS table and writing it to a road network graph
+ *
+ * @author Vikas Veshishth
+ * @author Philip Welch
+ * @author Mario Basa
+ * @author Robin Boldt
+ */
+public abstract class PostgisReader {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PostgisReader.class);
+
+    private final GraphHopperStorage graphStorage;
+    private final NodeAccess nodeAccess;
+    protected final Graph graph;
+    protected EncodingManager encodingManager;
+
+    private Map<String, String> postgisParams;
+
+    public PostgisReader(GraphHopperStorage ghStorage,
+                         Map<String, String> postgisParams) {
+
+        this.graphStorage = ghStorage;
+        this.graph = ghStorage;
+        this.nodeAccess = graph.getNodeAccess();
+        this.encodingManager = ghStorage.getEncodingManager();
+
+        this.postgisParams = postgisParams;
+    }
+
+    public void readGraph() {
+        graphStorage.create(1000);
+        processJunctions();
+        processRoads();
+        finishReading();
+    }
+
+    abstract void processJunctions();
+
+    abstract void processRoads();
+
+    /**
+     * This method will be called in the end to release the objects
+     */
+    protected abstract void finishReading();
+
+    protected FeatureIterator<SimpleFeature> getFeatureIterator(
+            DataStore dataStore, String tableName) {
+
+        if (dataStore == null)
+            throw new IllegalArgumentException("DataStore cannot be null for getFeatureIterator");
+
+        LOGGER.info("Getting the feature iterator for " + tableName);
+
+        try {
+            FeatureSource<SimpleFeatureType, SimpleFeature> source =
+                    dataStore.getFeatureSource(tableName);
+
+            Filter filter = getFilter(source);
+            FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures(filter);
+
+            FeatureIterator<SimpleFeature> features = collection.features();
+            return features;
+
+        } catch (Exception e) {
+            throw Utils.asUnchecked(e);
+        }
+    }
+
+    /**
+     * Filters can help a lot when you need to limit the results returned from PostGIS.
+     * A Filter can be used similar to the WHERE clause in regular SQL statements.
+     * It's easy to filter geometries that have a certain attributes, are in certain BBoxes, Polygons, etc.
+     * You can find a lot of sample filters here: https://github.com/geotools/geotools/blob/master/docs/src/main/java/org/geotools/main/FilterExamples.java
+     * <p>
+     * By default, all features are returned.
+     */
+    protected Filter getFilter(FeatureSource source) {
+        return Filter.INCLUDE;
+    }
+
+    protected DataStore openPostGisStore() {
+        try {
+            LOGGER.info("Opening DB connection to " + this.postgisParams.get("dbtype") + " " + this.postgisParams.get("host") + ":" + this.postgisParams.get("port") + " to database " + this.postgisParams.get("database") + " schema " + this.postgisParams.get("schema"));
+            DataStore ds = DataStoreFinder.getDataStore(this.postgisParams);
+            if (ds == null)
+                throw new IllegalArgumentException("Error Connecting to Database ");
+            return ds;
+
+        } catch (Exception e) {
+            throw Utils.asUnchecked(e);
+        }
+    }
+
+    /**
+     * This method can be used to filter features. One way to use it is to filter for features withing a certain BBox
+     *
+     * @return true if the feature should be accepted
+     */
+    protected boolean acceptFeature(SimpleFeature feature) {
+        return true;
+    }
+
+    /**
+     * Returns the coordinates of a feature.
+     */
+    protected List<Coordinate[]> getCoords(SimpleFeature feature) {
+        ArrayList<Coordinate[]> ret = new ArrayList<>();
+        if (feature == null)
+            return ret;
+        Object coords = feature.getDefaultGeometry();
+        if (coords == null)
+            return ret;
+
+        if (coords instanceof LineString) {
+            ret.add(((LineString) coords).getCoordinates());
+        } else if (coords instanceof MultiLineString) {
+            MultiLineString mls = (MultiLineString) coords;
+            int n = mls.getNumGeometries();
+            for (int i = 0; i < n; i++) {
+                ret.add(mls.getGeometryN(i).getCoordinates());
+            }
+        }
+
+        return ret;
+    }
+
+    /*
+     * Get longitude using the current long-lat order convention
+     */
+    protected double lng(Coordinate coordinate) {
+        return coordinate.getOrdinate(0);
+    }
+
+    /*
+     * Get latitude using the current long-lat order convention
+     */
+    protected double lat(Coordinate coordinate) {
+        return coordinate.getOrdinate(1);
+    }
+
+    protected void saveTowerPosition(int nodeId, Coordinate point) {
+        nodeAccess.setNode(nodeId, lat(point), lng(point));
+    }
+}
---
+++ reader-postgis/src/test/java/com/graphhopper/reader/postgis/OSMPostgisReaderTest.java
@@ -0,0 +1,114 @@
+/*
+ *  Licensed to GraphHopper GmbH under one or more contributor
+ *  license agreements. See the NOTICE file distributed with this work for
+ *  additional information regarding copyright ownership.
+ *
+ *  GraphHopper GmbH licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.graphhopper.reader.postgis;
+
+import com.graphhopper.GHRequest;
+import com.graphhopper.GHResponse;
+import com.graphhopper.GraphHopper;
+import com.graphhopper.GraphHopperConfig;
+import com.graphhopper.config.Profile;
+import com.graphhopper.util.Helper;
+import com.graphhopper.util.StopWatch;
+import com.graphhopper.util.shapes.GHPoint;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * To be able to use this test class, you need to define access to your postgis DB in the environment variables
+ * in your ~/.profile ~/.bashrc or similar
+ * <p>
+ * The current test uses a shape file from Geofabrik, this one: http://download.geofabrik.de/europe/andorra-201215-free.shp.zip
+ * I uploaded the file "gis_osm_roads_free_1.shp" to https://qgiscloud.com/ using qgis.
+ * <p>
+ * If you use a different shape file, you might need to change the test values.
+ *
+ * @author Robin Boldt
+ */
+public class OSMPostgisReaderTest {
+
+    private final String dir = "./target/tmp/test-db";
+
+    // Note: Change the number of expected edges to your database
+    private final int NR_EXPECTED_EDGES = 8538;
+
+    @Before
+    public void setUp() {
+        new File(dir).mkdirs();
+    }
+
+    @After
+    public void tearDown() {
+        Helper.removeDir(new File(dir));
+    }
+
+    @Test
+    public void testBasicGraphCreation() {
+
+        if (System.getenv("GH_DB_HOST") == null)
+            throw new IllegalStateException("You need to define the environment variables before running the test");
+
+
+        StopWatch stopWatch = new StopWatch().start();
+        //GraphHopper graphHopper = new GraphHopperPostgis().forServer();
+        GraphHopper graphHopper = new GraphHopperPostgis();
+
+        GraphHopperConfig graphHopperConfig = new GraphHopperConfig();
+        graphHopperConfig.putObject("db.host", System.getenv("GH_DB_HOST"));
+        graphHopperConfig.putObject("db.port", System.getenv("GH_DB_PORT"));
+        graphHopperConfig.putObject("db.database", System.getenv("GH_DB_DATABASE"));
+        graphHopperConfig.putObject("db.schema", System.getenv("GH_DB_SCHEMA"));
+        graphHopperConfig.putObject("db.user", System.getenv("GH_DB_USER"));
+        graphHopperConfig.putObject("db.passwd", System.getenv("GH_DB_PASSWD"));
+        graphHopperConfig.putObject("db.tags_to_copy", "name");
+
+        //TODO this should be fixed at some point, probably it would be better to have this in the args as well
+        graphHopperConfig.putObject("datareader.file", System.getenv("GH_DB_TABLE"));
+        graphHopperConfig.putObject("graph.location", dir);
+        graphHopperConfig.putObject("graph.flag_encoders", "car");
+
+        graphHopperConfig.setProfiles(Collections.singletonList(new Profile("my_car").setVehicle("car").setWeighting("fastest")));
+
+        graphHopper.init(graphHopperConfig);
+        //graphHopper.setCHEnabled(false);
+        graphHopper.importOrLoad();
+
+        assertEquals("The number of expected edges does not match", graphHopper.getGraphHopperStorage().getAllEdges().length(), NR_EXPECTED_EDGES);
+        stopWatch.stop();
+        System.out.println("Importing the database took: " + stopWatch.getSeconds());
+
+        GHRequest request = new GHRequest();
+        request.addPoint(new GHPoint(42.476655, 1.490536));
+        request.addPoint(new GHPoint(42.537271, 1.589928));
+        request.setProfile("my_car");
+        GHResponse response = graphHopper.route(request);
+
+        assertTrue(13000 < response.getBest().getDistance());
+        assertTrue(14000 > response.getBest().getDistance());
+
+        assertTrue(800000 < response.getBest().getTime());
+        assertTrue(900000 > response.getBest().getTime());
+    }
+
+}
